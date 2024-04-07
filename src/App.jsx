73493c26426221e30bf206a99af04292d410cd25
Shivam Kumar Gupta <115231608+Shivam-fibo@users.noreply.github.com>
2024-04-07 19:35:15 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <> 
+   
+      <div className="container">
+        <div className="birth">
+          <p>Birth Chart</p>
+          <hr className='uper-line'/>
+        </div>
+        <div className="rectangle">
+          <div className="cross-line"></div>
+          <div className="cross-line"></div>
+        <div className="diagonal-rectangle"></div>
+        </div>
+      </div>
+  
+    </>
+  )
+}
+
+export default App
